fa36162b8d508215a5683c25e767520524bf483a
Singein <singein@outlook.com>
2019-01-30 18:27:52 +0800

    feat: resorted foldertree

--- src/components/FolderTree/slots.js
+++ src/components/FolderTree/slots.js
@@ -2,17 +2,34 @@ var slots = {
   methods: {
     //打开设置窗口,点击设置按钮时触发
     slotUpdateTree(kwargs) {
-      kwargs.treeData[0].children.sort(function(s1, s2) {
+      kwargs.treeData[0].children.sort(function (s1, s2) {
         let x1 = s1.name.toUpperCase();
         let x2 = s2.name.toUpperCase();
-        if (x1 < x2) {
-          return -1;
+        if (s1.children !== undefined && s2.children !== undefined) {
+          if (x1 < x2)
+            return -1;
+
+          if (x1 > x2)
+            return 1;
+
+        } else if (s1.children === undefined && s2.children === undefined) {
+          if (x1 < x2)
+            return -1;
+
+          if (x1 > x2)
+            return 1;
+
+        } else {
+          if (s1.children !== undefined)
+            return -1;
+          else
+            return 1;
+
         }
-        if (x1 > x2) {
-          return 1;
-        }
+
         return 0;
       });
+
       this.data = kwargs.treeData;
       setTimeout(() => this.$send(this.SIGNAL_CLEAR(this)), 300);
     },
